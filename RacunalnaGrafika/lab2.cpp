c1ebf30d23c19e08728e6b9c8e1ecfa1d305f4ae
Josip Maretić <josip.maretic2011@outlook.com>
2023-10-18 12:52:25 +0200

    Add project files.

---
+++ RacunalnaGrafika/lab2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int lab2() {
+    std::cout << "Homework Task 2" << std::endl;
+    // Your task-specific code here
+    return 0;
+}
---
+++ RacunalnaGrafika/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int lab1();
+int lab2();
+
+int main() {
+
+    // LAB 1
+    //lab1();
+
+    // LAB 2
+    lab2();
+
+    return 0;
+}
